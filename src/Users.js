5b1edde61c758d803e38c2abc61243287bbcbf51
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-01-28 20:43:36 -0800

    fetched user data

--- src/Users.js
+++ src/Users.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+const API = 'https://acme-users-api-rev.herokuapp.com/api/users';
 
 function Users(){
-	return (<div>hiiiii</div>)
+
+	const [users, setUsers] = useState([]);
+	const [count, setCount] = useState(0);
+	const fetchUser = async() => {
+		const userAPI = `${API}${ count ? `/${count}` : ''}`;
+		console.log(userAPI);
+		return axios.get(userAPI).then(res => res.data)
+		}
+
+	useEffect(()=>{
+
+		fetchUser().then( res => { 
+			console.log(res.users);
+			setUsers(res.users) 
+			})
+
+		
+		
+	},[])
+
+	return (<ul>
+		hiiiii
+		</ul>)
 }
 
 export default Users;
